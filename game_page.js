8f0feee011d20d7c6381b386ee8bf8e85064b0c9
aahillohiya <120379091+aahillohiya@users.noreply.github.com>
2023-02-09 09:45:59 +0530

    Add files via upload

--- game_page.js
+++ game_page.js
@@ -33,3 +33,35 @@ function send() {
     answer_turn = "player2";
 }
 
+function check() {
+    if (atual_answer == number_1*number_2) {
+        if (answer_turn == "player1") {
+            player1_score = player1_score + 1;
+            document.getElementById("player1_score").innerHTML = player1_score ;
+        }
+        else{
+            player2_score = player2_score + 1;
+            document.getElementById("player2_score").innerHTML = player2_score ;
+        }
+    }
+
+     if (question_turn == "player1") {
+        question_turn = "player2";
+        document.getElementById("player_question").innerHTML = "Question turn " + player2_name ;
+    }
+    else{
+        question_turn = "player1";
+        document.getElementById("player_question").innerHTML = "Question turn " + player1_name ;
+    }
+
+    if (answer_turn == "player1") {
+        answer_turn = "player2";
+        document.getElementById("player_answer").innerHTML = "Answer turn " + player2_name ;
+    }
+    else{
+        answer_turn = "player1";
+        document.getElementById("player_answer").innerHTML = "Answer turn " + player1_name ;
+    }
+    
+    document.getElementById("output").innerHTML = "";
+}
